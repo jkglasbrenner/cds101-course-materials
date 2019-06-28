9f01cf7d2bb530cb3fb9ace3f916fb8398359860
James K. Glasbrenner <jglasbr2@gmu.edu>
2019-06-28 15:30:30 -0400

    Generate Pelican site

---
+++ theme/bib/auto/references.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "references"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Brauer:2007"
+    "Carter:2002"
+    "Newcomb:1882"
+    "Robinson:Cleaning:2015"
+    "hanley:2004"
+    "jaffe:1960"
+    "stigler:robust"))
+ :bibtex)
+
